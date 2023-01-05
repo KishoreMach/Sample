73b7c6974527268b5d7692fe3128f4d2bc5713ab
KishoreMach <62620783+KishoreMach@users.noreply.github.com>
2023-01-05 02:23:44 -0500

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,16 @@
+import com.examp.Application;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		Application myApp = new Application();
+		
+		String result = myApp.getStatus();
+	
+		assertEqua1s ("0K", reht) ;
+	}
+	
+}
